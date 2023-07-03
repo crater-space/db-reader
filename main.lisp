4d4828940e219a3465024a3194912f2781a6bad2
myTerminal <team.terminal@gmail.com>
2023-07-03 15:58:12 -0500

    implement 'list' command

--- main.lisp
+++ main.lisp
@@ -10,6 +10,19 @@
                                     while line
                                     collect line)))))
 
+;; Courtesy: https://gist.github.com/siguremon/1174988/babcbdcbbfcb9f42df34f000f9326a26caa64be4
+(defun split-string (string &optional (separator ",") (r nil))
+  "Splits a string around the (optionally) specified operator."
+  (let ((n (position separator string
+		             :from-end t
+		             :test #'(lambda (x y)
+			                   (find y x :test #'string=)))))
+    (if n
+	    (split-string (subseq string 0 n)
+                 separator
+                 (cons (subseq string (1+ n)) r))
+        (cons string r))))
+
 (defun command-get-available-source-names (known-sources)
   "Gathers a list of available package sources."
   (labels ((get-command-name-for-source (known-source)
@@ -47,14 +60,29 @@
   ;; TODO: Implement command
   )
 
-(defun command-list-packages ()
-  "Lists installed packages."
-  ;; TODO: Implement command
-  )
+(defun command-list-packages (known-sources sources-query)
+  "Lists installed packages from the supplied sources, using the known sources."
+  (labels ((get-source-by-name (source-name)
+             (find source-name known-sources :key #'car :test #'string-equal))
+           (get-list-command-for-source (source)
+             (cadar (remove-if-not (lambda (property-pair)
+                                     (and (listp property-pair)
+                                          (eql (car property-pair) :LIST)))
+                                   (cdr source)))))
+    (let* ((relevant-source-names (split-string sources-query ",")))
+      (princ (reduce (lambda (a b)
+                       (concatenate 'string
+                                    a
+                                    "
+"
+                                    b))
+                     (mapcar (lambda (source-name)
+                               (get-list-command-for-source (get-source-by-name source-name)))
+                             relevant-source-names)
+                     :initial-value "")))))
 
 (defun command-install-packages (packages)
   "Installs the specified packages."
-  ;; TODO: Implement command
   )
 
 (defun command-uninstall-packages (packages)
@@ -74,7 +102,7 @@
          (known-sources (file-to-string "../db/sources.lisp")))
     (cond ((string-equal command-name "get_sources") (command-get-available-source-names known-sources))
           ((string-equal command-name "search") (princ "Not implemented!"))
-          ((string-equal command-name "list") (princ "Not implemented!"))
+          ((string-equal command-name "list") (command-list-packages known-sources (cadr arguments)))
           ((string-equal command-name "install") (princ "Not implemented!"))
           ((string-equal command-name "uninstall") (princ "Not implemented!"))
           ((string-equal command-name "update") (princ "Not implemented!"))
