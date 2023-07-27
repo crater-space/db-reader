25b1823193cb32a502f7c0011e722b0e827ef469
myTerminal <team.terminal@gmail.com>
2023-07-26 23:14:50 -0500

    write tests for concatenation functions

--- lib-tests.lisp
+++ lib-tests.lisp
@@ -8,4 +8,15 @@
 (test concatenates-on-same-line
       (is (string-equal (concatenate-on-same-line "a" "b")
                         "ab")
-          "two strings are concatenated without a delimiter"))
+          "two strings are concatenated without a delimiter")
+      (is (string-equal (concatenate-on-same-line "a" nil)
+                        "a")
+          "the only argument is returned"))
+
+(test concatenates-by-spaces
+      (is (string-equal (concatenate-by-spaces "a" "b")
+                        "a b")
+          "two strings are concatenated with a space as a delimiter")
+      (is (string-equal (concatenate-by-spaces "a" nil)
+                        "a ")
+          "the only argument is returned, followed by a space"))
